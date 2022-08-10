a14b32cf217e1ed3bcb10328f8a1a6c054cc3093
a-velazquez <a.m.velazquez@wustl.edu>
2022-08-10 14:25:37 -0500

    add day 3 lab items

--- Day3/Lab/lab03.py
+++ Day3/Lab/lab03.py
@@ -11,23 +11,62 @@
 
 ## make all characters capitalized
 def shout(txt):
+    if type(txt) != str:
+        raise TypeError(f"{txt} is not a string.")
+    else:
+        return txt.upper()
 
 
 ## reverse all characters in string
 def reverse(txt):
+    if type(txt) != str:
+        raise TypeError(f"{txt} is not a string.")
+    else:
+        return txt[::-1]
 
 
 ## reverse word order in string
 def reversewords(txt):
+    if type(txt) != str:
+        raise TypeError(f"{txt} is not a string.")
+    else:
+        word_list = txt.split()
+        return " ".join(word_list[::-1])
 
 
 ## reverses letters in each word
 def reversewordletters(txt):
+    if type(txt) != str:
+        raise TypeError(f"{txt} is not a string.")
+    else:
+        rev_words = []
+        for word in txt.split():
+            rev_words.append(word[::-1])
+        return " ".join(rev_words)
 
 
 ## optional -- change text to piglatin.. google it!
 def piglatin(txt):
+    if type(txt) != str:
+        raise TypeError(f"{txt} is not a string.")
+    else:
+        letters = ["{:c}".format(i) for i in range(97, 123)]
+        vowels = ["a", "e", "i", "o", "u"]
+        consonants = [l for l in letters if l not in vowels]
 
+        word_list = txt.split()
+        pl = []
+        for word in word_list:
+            if word[0] in consonants:
+                if word[1] in consonants:
+                    pl_word = word[2:] + "-" + word[0:2] + "ay"
+                else:
+                    pl_word = word[1:] + "-" + word[0] + "ay"
+            else:
+                pl_word = word + "-yay"
+            pl.append(pl_word)
+
+        return " ".join(pl)
 
 
 ## Try/catch is more common when using
@@ -36,15 +75,12 @@ def piglatin(txt):
 ## Loop over this string and apply the reverse() function.
 ## Should throw errors if your exceptions are being raised!
 ## Write a try/catch to handle this.
- 
+
 string_list = ["hi", "hello there", 5, "hope this works", 100, "will it?"]
 
-
-		
-			
-			
-			
-			
-			
-			
-
+for item in string_list:
+    try:
+        print(reverse(item))
+    except TypeError:
+        print("A string was not passed")
+        pass
--- Day3/Lab/lab03_test.py
+++ Day3/Lab/lab03_test.py
@@ -1,22 +1,50 @@
 import unittest
+import os
+
+os.chdir("./Documents/GitHub/python_summer2022/Day3/Lab")
+
 from lab03 import *
 
+
 class labTests(unittest.TestCase):
-	
-	## fill in a few tests for each
-	## make sure to account for correct and incorrect input
+
+    ## fill in a few tests for each
+    ## make sure to account for correct and incorrect input
 
     def test_shout(self):
+        self.assertEqual(shout("foo"), "FOO")
+        w = 5
+        with self.assertRaises(TypeError):
+            shout(w)
 
     def test_reverse(self):
+        self.assertEqual(reverse("foo"), "oof")
+        self.assertEqual(reverse("here it is"), "si ti ereh")
+        w = 5
+        with self.assertRaises(TypeError):
+            reverse(w)
 
     def test_reversewords(self):
+        self.assertEqual(reversewords("here it is"), "is it here")
+        self.assertEqual(reversewords("foo"), "foo")
+        w = 5
+        with self.assertRaises(TypeError):
+            reversewords(w)
 
     def test_reversewordletters(self):
+        self.assertEqual(reversewordletters("here it is"), "ereh ti si")
+        self.assertEqual(reversewordletters("foo"), "oof")
+        w = 5
+        with self.assertRaises(TypeError):
+            reversewordletters(w)
 
     def test_piglatin(self):
+        self.assertEqual(piglatin("foo"), "oo-fay")
+        self.assertEqual(piglatin("foo bar"), "oo-fay ar-bay")
+        w = 5
+        with self.assertRaises(TypeError):
+            piglatin(w)
 
 
-if __name__ == '__main__':
-  unittest.main()
-
+if __name__ == "__main__":
+    unittest.main()
