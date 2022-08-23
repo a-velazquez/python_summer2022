53fdf681c958426aa113a6ab29e69ac8d91adeb7
a-velazquez <a.m.velazquez@wustl.edu>
2022-08-23 16:37:13 -0500

    add day 9 lab responses

--- Day9/Lab/lab09.py
+++ Day9/Lab/lab09.py
@@ -3,28 +3,29 @@
 # EXAMPLE: link nodes into a ring
 # Function to link 2 nodes (not in the sense of last script)
 def makeLink(G, node1, node2):
-  if node1 not in G:
-    G[node1] = {}
-  G[node1][node2] = True
-  if node2 not in G:
-    G[node2] = {}
-  G[node2][node1] = True
-  return G 
+    if node1 not in G:
+        G[node1] = {}
+    G[node1][node2] = True
+    if node2 not in G:
+        G[node2] = {}
+    G[node2][node1] = True
+    return G
+
 
 graph = {}
 graph = makeLink(graph, "a", "b")
 graph
 
-# empty graph 
-ring = {} 
+# empty graph
+ring = {}
 
-# number of nodes 
-n = 5 
+# number of nodes
+n = 5
 
 # Add in edges with makeLink function
 for i in range(n):
-  ring = makeLink(ring, i, (i+1)%n)
-  print(ring)
+    ring = makeLink(ring, i, (i + 1) % n)
+    print(ring)
 
 print(ring)
 # 4 = 0 = 1 = 2 = 3 = 4
@@ -33,34 +34,55 @@ print(ring)
 print(len(ring))
 
 # How many edges?
-print(sum([len(ring[node]) for node in ring.keys()])/2)
+print(sum([len(ring[node]) for node in ring.keys()]) / 2)
 
 
 ## Grid Network
 ## TODO: create a square graph with 9 nodes using the makeLink function (from above)
 ## Example: https://www.mathworks.com/matlabcentral/answers/213955-how-to-determine-the-neighbours-of-each-node-in-a-square-graph
 
+
+def make_square(n_nodes):
+
+    sq_graph = {}
+
+    tree_width = int(math.sqrt(n_nodes))
+
+    for number in range(1, n_nodes + 1):
+        if number + 3 <= n_nodes:
+            sq_graph = makeLink(sq_graph, number, number + tree_width)
+        if (number + 1) <= n_nodes and number % tree_width != 0:
+            sq_graph = makeLink(sq_graph, number, number + 1)
+        if (number - 1) == 1:
+            sq_graph = makeLink(sq_graph, number, number + 1)
+
+    return sq_graph
+
+
 ## TODO: define a function countEdges
-
 # You may want to use the module math
-import math 
+import math
 
 
+def countEdges(graph):
+    links = []
+    for node, connections in graph.items():
+        for node_name, truth_val in connections.items():
+            if {node, node_name} not in links:
+                links.append({node, node_name})
 
-
-
-
-
+    return len(links)
 
 
 ##  Social Network
 # some set-up:
 class Actor(object):
-  def __init__(self, name):
-    self.name = name 
+    def __init__(self, name):
+        self.name = name
 
-  def __repr__(self):
-    return self.name 
+    def __repr__(self):
+        return self.name
+
 
 ss = Actor("Susan Sarandon")
 jr = Actor("Julia Roberts")
@@ -72,17 +94,16 @@ dh = Actor("Dustin Hoffman")
 
 movies = {}
 
-movies = makeLink(movies, dh, rd) # Wag the Dog
-movies = makeLink(movies, rd, ms) # Marvin's Room
-movies = makeLink(movies, dh, ss) # Midnight Mile
-movies = makeLink(movies, dh, jr) # Hook
-movies = makeLink(movies, dh, kb) # Sleepers
-movies = makeLink(movies, ss, jr) # Stepmom
-movies = makeLink(movies, kb, jr) # Flatliners
-movies = makeLink(movies, kb, ms) # The River Wild
-movies = makeLink(movies, ah, ms) # Devil Wears Prada
-movies = makeLink(movies, ah, jr) # Valentine's Day
-
+movies = makeLink(movies, dh, rd)  # Wag the Dog
+movies = makeLink(movies, rd, ms)  # Marvin's Room
+movies = makeLink(movies, dh, ss)  # Midnight Mile
+movies = makeLink(movies, dh, jr)  # Hook
+movies = makeLink(movies, dh, kb)  # Sleepers
+movies = makeLink(movies, ss, jr)  # Stepmom
+movies = makeLink(movies, kb, jr)  # Flatliners
+movies = makeLink(movies, kb, ms)  # The River Wild
+movies = makeLink(movies, ah, ms)  # Devil Wears Prada
+movies = makeLink(movies, ah, jr)  # Valentine's Day
 
 
 def findPath(graph, start, end, path=[]):
@@ -105,51 +126,72 @@ def findPath(graph, start, end, path=[]):
 
 print(findPath(movies, jr, ms))
 
-## start with julia roberts 
+## start with julia roberts
 ## who is she directly connected to?
 movies[jr].keys()
 ## who are they connected to?
-movies[ss].keys() 
-movies[ah].keys() ## found meryl streep!
+movies[ss].keys()
+movies[ah].keys()  ## found meryl streep!
 movies[dh].keys()
-movies[kb].keys() ## found meryl streep!
+movies[kb].keys()  ## found meryl streep!
 ## so shortest path is either
 ## jr -- ah -- ms
 ## jr -- kb -- ms
 
 
-
-
 ## TODO: implement findAllPaths() to find all paths between two nodes
-## allPaths = findAllPaths(movies, jr, ms)
-## for path in allPaths:
-##   print path
 
 
+def findAllPaths(graph, start, end, path=[]):
+
+    path = path + [start]
+    ## base case, reached end
+    if start == end:
+        return [path]
+
+    if start not in graph:
+        return None
+
+    all_paths = []
+    ## for each connection to starting node
+    for node in graph[start]:
+        ## check if it is already in path
+        if node not in path:
+            all_paths.extend(findAllPaths(graph, node, end, path))
+
+    # all_paths = filter(None, all_paths)
+    return all_paths
 
 
+allPaths = findAllPaths(movies, jr, ms)
+
+for i, path in enumerate(allPaths):
+    print(i, path)
 
 
 ## TODO: implement findShortestPath() to print shorest path between actors
-## print findShortestPath(movies, ms, ss)
 
 
+def findShortestPath(graph, start, end):
+    l = findAllPaths(graph, start, end)
+    return min(l, key=len)
 
 
+print(findShortestPath(movies, ms, ss))
 
 
 # Copyright (c) 2014 Matt Dickenson
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
